fb5ae74eb89f34683c52912886eb779284d225d5
Leif Coleman <mageleif@yahoo.com>
2017-01-10 17:19:43 -0600

    Update files

--- node/post.js
+++ node/post.js
@@ -2,14 +2,32 @@ module.exports = function(app){
   //////////////////////////////////
   // Include local libraries here //
   //////////////////////////////////
-  var lib = require("./func-parser.js");
+  var testing = require("testing");
 
-  ////////////////
-  // add-person //
-  ////////////////
-  app.post("/add-person", function(req, res){
-    lib.AppendToFile("data/people.csv", req.body);
-    var people = lib.CsvToObject("data/people.csv");
+  ///////////
+  // query //
+  ///////////
+  app.post("/query", function(req, res){
+    console.log(req);
+
+    // Do something
+    var username = "TundraFizz";
+    var key = "remember_82e5d2c56bdd0811318f0cf078b78bfc";
+    var val = "eyJpdiI6IkZUblM0ZlZyWXFqeXVSdGY5aWZJYXc9PSIsInZhbHVlIjoiWllSZ1YzK1ZnSFRxNXI0WTdpTm53YjgyNkxlTFhXM041UW9YU1lxXC9iUXNYUytYTk5pWlF5am9YWEswc0ZySDhGWE5aQnpBcXRNcmRsb1c2cytCak1ZWlVQTktUNGp3ZjdsaUtwb1wvRHVEZz0iLCJtYWMiOiI4ZTUxNjc1NDM5OThmMjYwNmE2ZjVkOGYwNTdhM2UzYzY5NDQxOGUzZWMxNDY5MWMzODFlNTMyNWIyYzIzZjcwIn0";
+
+    var sample1 = new testing.CreateObject(username, key, val);
+
+    searchTerm = "yolo";
+
+    sample1.SearchSomething(searchTerm, function(body){
+      body = JSON.stringify(body, undefined, 2);
+
+      res.render("index.ejs", {
+        data: body
+      });
+    });
+
+
     res.json(people);
   });
 }
--- node/routes-404.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app){
-  //////////////////////////////////
-  // 404: No route or file exists //
-  //////////////////////////////////
-  app.use(function (req, res){
-    res.render("404.ejs");
-  });
-}
